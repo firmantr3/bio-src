5bbb6f87071f1060ea195223e13a06f67e0c04d8
Firman Taruna Nugraha <firmantr3@gmail.com>
2019-09-21 15:13:45 +0700

    fix test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,32 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import store from '@/store'
+import i18n from '@/plugins/i18n'
+import router from '@/router'
+import Navbar from '@/components/Navbar.vue'
+import Welcome from '@/pages/welcome.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
+import '@/plugins'
+import '@/components'
+
+const localVue = {
+  i18n,
+  store,
+  router
+}
+
+describe('Navbar.vue', () => {
+  it('renders APP_NAME global variable', () => {
+    const brand = APP_NAME
+    const wrapper = shallowMount(Navbar, { ...localVue })
+    expect(wrapper.text()).to.include(brand)
+  })
+})
+
+describe('welcome.vue', () => {
+  it('renders APP_NAME global variable', () => {
+    const brand = APP_NAME
+    const wrapper = shallowMount(Welcome, { ...localVue })
+    expect(wrapper.text()).to.include(brand)
   })
 })
