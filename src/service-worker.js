539e6e01dd326c8233d3f18aad78d57b3f3d752f
Firman Taruna Nugraha <firmantr3@gmail.com>
2020-02-15 21:07:59 +0700

    precache my avatar

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,11 +5,18 @@ if(workbox) {
         return ! isImage;
     });
 
+    precacheManifest.concat([
+        {
+            "revision": "674f50d287a8c48dc19ba404d20fe713",
+            "url": "https://gravatar.com/avatar/85c9a1594c2d324183364fd303ba0ae4?s=300"
+        }
+    ]);
+
     workbox.precaching.precacheAndRoute(precacheManifest, {});
 
     workbox.routing.registerRoute(
-        /\.(?:png|gif|jpg|jpeg|svg)$/,
-        workbox.strategies.staleWhileRevalidate({
+        new RegExp(/\.(?:png|gif|jpg|jpeg|svg)$/),
+        new workbox.strategies.CacheFirst({
             cacheName: 'images',
             plugins: [
                 new workbox.expiration.Plugin({
@@ -39,9 +46,9 @@ if(workbox) {
     //     }),
     // );
 
-    self.addEventListener("message", (e) => {
+    this.addEventListener("message", (e) => {
         console.log('updated, skip waiting!')
-        if (e.data.action == 'skipWaiting') self.skipWaiting()
+        if (e.data.action == 'skipWaiting') this.skipWaiting()
     })
 }
 else {
