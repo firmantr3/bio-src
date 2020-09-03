8c5de0e55f374a9c21c38b2440781734828ca911
Firman Taruna Nugraha <firmantr3@gmail.com>
2020-09-03 08:42:53 +0700

    fix: service worker deprecated

--- src/service-worker.js
+++ src/service-worker.js
@@ -17,13 +17,7 @@ if(workbox) {
     workbox.routing.registerRoute(
         new RegExp(/\.(?:png|gif|jpg|jpeg|svg)$/),
         new workbox.strategies.CacheFirst({
-            cacheName: 'images',
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 60,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                }),
-            ],
+            cacheName: 'images'
         }),
     );
 
