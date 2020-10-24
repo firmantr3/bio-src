f8c6960e4ee61874c1bc8455fbe3e6a068d135fa
Firman Taruna Nugraha <firmantr3@gmail.com>
2020-10-24 10:43:55 +0700

    optimize bootstrap vue

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,4 +1,22 @@
-import BootstrapVue from 'bootstrap-vue'
+import {
+  NavbarPlugin,
+  DropdownPlugin,
+  CardPlugin,
+  ImagePlugin,
+  LayoutPlugin,
+  ModalPlugin,
+  CollapsePlugin,
+  BadgePlugin,
+  VBScrollspy
+} from 'bootstrap-vue'
 import Vue from 'vue'
 
-Vue.use(BootstrapVue)
+Vue.use(LayoutPlugin);
+Vue.use(NavbarPlugin);
+Vue.use(DropdownPlugin);
+Vue.use(CardPlugin);
+Vue.use(ImagePlugin);
+Vue.use(ModalPlugin);
+Vue.use(CollapsePlugin);
+Vue.use(BadgePlugin);
+Vue.directive('b-scrollspy', VBScrollspy)
