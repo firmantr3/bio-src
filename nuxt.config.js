7a561c76684272cde2408debd6de57a996b36855
Firman Taruna Nugraha <firmantr3@gmail.com>
2023-03-06 10:43:25 +0700

    fix metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,14 +6,13 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'bio-src',
+    title: 'Firman Taruna Nugraha',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
@@ -53,6 +52,12 @@ export default {
 
   gtm: {
     id: 'UA-161599854-1'
+  },
+
+  pwa: {
+    manifest: {
+      name: 'Firman Taruna Nugraha',
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
